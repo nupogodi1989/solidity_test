90c27313de9043aed8fca864b4ac1736ef2d875d
Boris Sarkisov <basarkis@alumni.unca.edu>
2017-06-23 10:48:56 -0400

    Went through solidity example contracts and copied them

---
+++ contracts/Coin.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.0
+
+contract Coin {
+    // the keyword "public" makes those variables
+    // readable from the outside.
+    address public minter;
+    mapping (address => uint) public balances;
+
+    // Events allow light clients to react on
+    // changes efficiently.
+    event Sent(address from, address to, uint amount);
+
+    function Coin() {
+        minter = msg.sender;
+    }
+
+    function mint(address receiver, uint amount) {
+        if (msg.sender != minter) return;
+        balances[receiver] += amount;
+    }
+
+    function send(address receiver, uint amount) {
+        if (balances[msg.sender] < amount) return;
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        Sent(msg.sender, receiver, amount);
+    }
+}
